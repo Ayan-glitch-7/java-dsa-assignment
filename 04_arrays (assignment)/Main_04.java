8d5c8f95d8d4260f06c1147c6413195d1fd11b33
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-04-01 14:22:32 +0000

    c

---
+++ 04_arrays (assignment)/Main_04.java
@@ -0,0 +1,17 @@
+class Main_04 {
+    public int maximumWealth(int[][] accounts) {
+
+        int wealth = 0;
+
+        for (int i = 0; i < accounts.length; i++) {
+            int money = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                money = money + accounts[i][j];
+            }
+            if (money > wealth) {
+                wealth = money;
+            }
+        }
+        return wealth;
+    }
+}
