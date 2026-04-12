45db9ff9d04a36ea87e9815086a26c8579ecaa18
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-04-12 06:52:37 +0000

    c

---
+++ 04_arrays (assignment)/Main_23.java
@@ -0,0 +1,33 @@
+/* LEETCODE (# 1380) */
+
+
+class Main_23 {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            int minValue = matrix[i][0];
+            int columnIndex = 0;
+
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][j] < minValue) {
+                    minValue = matrix[i][j];
+                    columnIndex = j;
+                }
+            }
+
+            boolean isLucky = true;
+            for (int k = 0; k < matrix.length; k++) {
+                if (matrix[k][columnIndex] > minValue) {
+                    isLucky = false;
+                }
+            }
+
+            if (isLucky == true) {
+                ans.add(minValue);
+            }
+        }
+        return ans;
+    }
+}
