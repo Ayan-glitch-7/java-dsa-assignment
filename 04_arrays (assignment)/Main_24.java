0894f595b6137a78c51e6b5c50376ec527eac929
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-04-12 08:03:43 +0000

    c

---
+++ 04_arrays (assignment)/Main_24.java
@@ -0,0 +1,17 @@
+/* LEETCODE (# 53) */
+
+
+
+class Main_24 {
+    public int maxSubArray(int[] nums) {
+        
+        int currentSum = nums[0];
+        int maxSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(currentSum, maxSum);
+        }
+        return maxSum;
+    }
+}
